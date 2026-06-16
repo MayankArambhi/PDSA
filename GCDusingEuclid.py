c9f3a96604d9a55d9aacbfcc4fc5d7b29abb0478
MayankArambhi <0003mayank@gmail.com>
2026-06-16 09:49:09 +0530

    Initial commit

---
+++ GCDusingEuclid.py
@@ -0,0 +1,14 @@
+# suppose n does not divide m
+# m = qn + r
+# d divides both m and n
+# m = ad, n = bd
+# m = qn+r => ad = q(bd) + r
+# r must be form of cd
+
+def gcd(m,n):
+    a, b = (max(m,n),min(m,n))
+    if a%b == 0:
+        return b
+    return gcd(b,a%b)
+
+print(gcd(245,125))
